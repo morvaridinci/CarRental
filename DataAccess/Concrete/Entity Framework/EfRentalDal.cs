19df9dc0b7a0c4487fb69494e557a88773875bf5
morvaridincikayik <morvaridincikayik@gmail.com>
2021-02-14 15:34:03 +0200

    10.gun 4.odev

---
+++ DataAccess/Concrete/Entity Framework/EfRentalDal.cs
@@ -0,0 +1,42 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.Entity_Framework
+{
+    public class EfRentalDal : EfEntityRepositoryBase<Rental, MyDatabaseContext>, IRentalDal
+    {
+        public List<RentalDetailDto> GetRentalDetails(Expression<Func<Rental, bool>> filter = null)
+        {
+            using (MyDatabaseContext context = new MyDatabaseContext())
+            {
+                var result = from r in filter is null ? context.Rentals : context.Rentals.Where(filter)
+                             join c in context.Cars
+                             on r.CarId equals c.CarId
+                             join cu in context.Customers
+                             on r.CustomerId equals cu.Id
+                             join u in context.Users
+                             on cu.UserId equals u.Id
+                             select new RentalDetailDto
+                             {
+                                 Id = r.Id,
+                                 CarName = c.CarName,
+                                 CustomerName = cu.CompanyName,
+                                 CarId = c.CarId,
+                                 RentDate = r.RentDate,
+                                 ReturnDate = r.ReturnDate,
+                                 
+                             };
+
+                return result.ToList();
+
+            }
+        }
+    }
+}
